33616378b74a577760ba273fdd192d71a6904024
reroque <reroque@cisco.com>
2020-07-07 21:54:45 -0700

    saving

--- programming_languages/python/binary_search-high_low_game.py
+++ programming_languages/python/binary_search-high_low_game.py
@@ -11,9 +11,16 @@ print("*" * 40)
 
 low = int(input("Enter low number: "))
 high = int(input("Enter high number: "))
+if (low == high):
+    print("Too easy, its {}!".format(low))
+    exit(0)
+elif (low > high):
+    print("Can't trick me, low is now {} and high is now {}".format(high, low))
+    temp = low
+    low = high
+    high = temp
 max_of_guesses = int(math.log(high - low,2) + 1)
-answer = int(input("Enter number between {0} and {1}.\n" \
-                   "Computer will guess the number in {2} guesses.\n".format(low, high, max_of_guesses)))
+print("Write your number down, I will guess the number in {0} guesses.\n".format(max_of_guesses))
 
 def guess_a_number(lo, hi):
     return lo + ((hi - lo) // 2)
@@ -22,14 +29,13 @@ while (True):
     guess = guess_a_number(low, high)
     guesses.append(guess)
     num_of_guesses += 1
-    if (guess < answer):
-        #print("It is not {}. Guess higher".format(guess))
+    high_low = input("I guess {0}.  Enter c (correct), h (higher), l (lower)".format(guess))
+    if (high_low == 'h'):
         low = guess + 1
-    elif (guess > answer):
-        #print("It is not {}. Guess lower".format(guess))
+    elif (high_low == 'l'):
         high = guess - 1
     else:
-        #print("Nice, you guessed it, the number is {}".format(answer))
+        print("See! I told you I can guess the number in {} guesses!".format(max_of_guesses))
         print("Number of guesses: ", num_of_guesses)
         print("Computer guesses were: ", guesses)
         break
