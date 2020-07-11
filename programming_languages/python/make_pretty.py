8e3d6636346669bbdf27a6903153bcd54fc105bb
reroque <reroque@cisco.com>
2020-07-11 15:08:28 -0700

    saving

---
+++ programming_languages/python/make_pretty.py
@@ -0,0 +1,8 @@
+def header(string):
+    size_of_string = len(string)
+    border = "*" * size_of_string
+    return(border + "\n" + string + "\n" + border)
+
+def commands():
+    border = "*" * 3
+    return(border)
