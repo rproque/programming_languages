723b275c730f376dfeb94956907cbb352e814d7d
reroque <reroque@cisco.com>
2020-07-14 20:30:36 -0700

    saving

--- programming_languages/python/prime_finder.py
+++ programming_languages/python/prime_finder.py
@@ -1,11 +1,12 @@
 #!/usr/local/bin/python3
 #MacOSX
 # Check if a number is a prime number:
-## A number is prime if it is divisible by exactly 2 distinct positive numbers 1 and the number itself.
-## https://www.mathsisfun.com/prime-composite-number.html
+# A number is prime if it is divisible by exactly 2 distinct positive numbers 1 and the number itself.
+# https://www.mathsisfun.com/prime-composite-number.html
+# also: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+# wow: https://leetcode.com/problems/count-primes/discuss/379169/Python-different-solutions
+
 import argparse
-
-
 def is_prime(n):
     for x in range(2, n + 1):
         if (x != n):
@@ -27,22 +28,15 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-n', '--number', type=int)
     args = parser.parse_args()
-    number = args.number
+    number = 10
 
     print("PRIME TOOL")
     print("Definition: A number is prime if it is divisible by only"
           " 2 distinct positive numbers: 1 and the number itself.")
-    # if is_prime(number):
-    #     print("{} is a prime number.".format(number))
-    # else:
-    #     print("{} is not a prime number.".format(number))
+    if is_prime(number):
+        print("{} is a prime number.".format(number))
+    else:
+        print("{} is not a prime number.".format(number))
 
-   # list = find_prime(number)
-    #print("List of Prime Numbers: ", ' '.join([str(num) for num in list]))
-
-    x = 1
-    i = 1
-    while (x > 0):
-        x /= 2
-        print("Loop {}: x = {}".format(i, x))
-        i += 1
+    list = find_prime(number)
+    print("List of Prime Numbers: ", ' '.join([str(num) for num in list]))
